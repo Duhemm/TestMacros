e5a73c65787d2dbaeea697ce7bd3a02931b7ee12
Martin Duhem <martin@c-webhosting.org>
2013-12-25 18:14:19 +0100

    The macro still does not support recursive case classes such as :  - case class A(x: B)    case class B(x: A)
    But supports case class A(x: A)

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -3,7 +3,8 @@ case class B(x: Int, y: String)
 case class C(x: Boolean)
 case class D(x: A, y: B, z: C)
 case class E(x: E)
-//case class F(x: E)
+case class F(x: G)
+case class G(x: F)
 
 object Test extends App {
 
@@ -11,30 +12,33 @@ object Test extends App {
   import reflect.api.Liftable
   import scala.reflect.runtime.universe._
   import scala.reflect.runtime.currentMirror
-  
+
   val c1 = A(18)
   val c2 = B(1, "Second value")
   val c3 = C(true)
   val c4 = D(c1, c2, c3)
   val c5 = E(E(E(E(E(E(E(null)))))))
-  //val c5 = E(F(E(null)))
+  val c6 = F(G(null))
 
-  /*val r1 = q"$c1"
-  val r2 = q"$c2"
-  val r3 = q"$c3"
-  val r4 = q"$c4"*/
+  //  val r1 = q"$c1"
+  //  val r2 = q"$c2"
+  //  val r3 = q"$c3"
+  //  val r4 = q"$c4"
   val r5 = q"$c5"
-  
-  /*println("RAW : " + showRaw(r1))
-  println("RAW : " + showRaw(r2))
-  println("RAW : " + showRaw(r3))
-  println("RAW : " + showRaw(r4))*/
-  println("RAW : " + showRaw(r5))
+  //  val r6 = q"$c6"
 
-  /*println(r1)
-  println(r2)
-  println(r3)
-  println(r4)*/
+  //  println("RAW : " + showRaw(r1))
+  //  println("RAW : " + showRaw(r2))
+  //  println("RAW : " + showRaw(r3))
+  //  println("RAW : " + showRaw(r4))
+  println("RAW : " + showRaw(r5))
+  //  println("RAW : " + showRaw(r6))
+
+  //  println(r1)
+  //  println(r2)
+  //  println(r3)
+  //  println(r4)
   println(r5)
-  
+  //  println(r6)
+
 }
